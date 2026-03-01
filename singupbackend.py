15e89b31ed0fae81f6c2fc8343b15fac11c9699e
SheikhShameel <alwaysshameel@gmail.com>
2026-03-01 13:41:01 +0000

    the final boss in website

--- singupbackend.py
+++ singupbackend.py
@@ -641,4 +641,4 @@ def my_scans():
 # RUN
 # ═══════════════════════════════════════════════════════════════
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
